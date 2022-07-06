5c66a78db59f337bfc3d0ea878e884298fe71319
sonramos <jackson.ramoos@outlook.com>
2022-07-06 00:22:00 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 import flags from './model/flags.js';
+
+function loadFlags(){
+    const flagBase = flags;
+
+    for (const flag of flagBase){
+        const flagView = document.querySelector(".flag-view");
+
+        const flagCard = showFlag(flag);
+
+        flagView.insertAdjacentHTML("beforeend", flagCard);
+    }
+
+}
+
+function showFlag(flag){
+    return `<div class="flag col-2 my-2 text-center">
+    <img src="${flag.image}" alt="${flag.name}">
+    <p>${flag.name}}</p>
+  </div>`
+}
+
+loadFlags()
